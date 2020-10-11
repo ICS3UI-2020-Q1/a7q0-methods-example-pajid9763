51a1f7fd661f04efd560376e59d73a8d8280f6d0
pajid9763 <pajid9763@wrdsb.ca>
2020-10-11 16:27:53 +0000

    finished methods example

--- Main.java
+++ Main.java
@@ -3,13 +3,33 @@
  * @author 
  */
 public class Main {
+  // create a method to say hello
+  public static void sayHello(){
+    System.out.println("Hello World");
+  }
+
+  // saying hello to a specific person
+  public static void sayHello(String name){
+    System.out.println("Hello " + name);
+  }
+
+  // a funtion that will calculate the area of a rectangle
+  public static double calcAreaofRectangle (double length, double width){
+    double area = length * width;
+    return area;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // call out method sayHello
+    sayHello();
+
+    // say hello to Steve 
+    String firstName = "Steve";
+    sayHello(firstName);
     
   }
 }
